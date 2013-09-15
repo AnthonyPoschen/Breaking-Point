9638a04dd3854f6e9b956051e754f7dd0f92e42c
zanven42 <anthony_@live.com.au>
2013-09-16 01:55:44 +1000

    Transform - FIX
    fixed transform math to do parent child multiplies properly by using
    Matrix3 for rotation. vec3 (float3) for position. and float for scale.
    saving vast memory.
    
    should consider extending quaternion class to allow the abillity to not
    use a 3x3 matrix for roation and instead use a quaternion. tradeofs will
    have to be calculated. but quaternions seem like the better option all
    round.

--- src/Core/Transform.cpp
+++ src/Core/Transform.cpp
@@ -30,8 +30,17 @@ Transform::~Transform()
 Transform Transform::operator * (const Transform& a_kTran) const
 {
 	Transform kResult;
-	kResult.m_kRotate = this->m_kRotate * a_kTran.m_kRotate;
-	kResult.m_kTranslate = this->m_kTranslate + a_kTran.m_kTranslate;
+	matrix4 omat;
+	// this = parent
+	// a_kTran = child
+	
+	kResult.m_kTranslate = this->m_kRotate * a_kTran.m_kTranslate;
+	kResult.m_kTranslate += this->m_kTranslate;
+
+	
+	kResult.m_kRotate = a_kTran.m_kRotate * this->m_kRotate;
 	kResult.m_fScale = this->m_fScale * a_kTran.m_fScale;
+	
 	return kResult;
+
 }
